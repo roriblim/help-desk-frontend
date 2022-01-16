fffce0516d66da0fdebe41dfd8cc52bf88b819cd
rosana <rosanariblim@gmail.com>
2022-01-16 14:25:45 -0300

    rota para TECNICO-LIST

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
   {
-    path:'', component: NavComponent, children:[{path:'home', component: HomeComponent},]
+    path:'', component: NavComponent, children:[
+      {path:'home', component: HomeComponent},
+      {path:'tecnicos',component:TecnicoListComponent}
+    ]
 
     /*vamos colocar a rota de home como filha da rota de navcomponent, a fim de que 
     o router-outlet utilizado seja o de navcomponent. Se não houver router-outlet em navcomponent, 
