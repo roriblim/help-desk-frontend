71fc676090043f00a41ceccfb6cc698d634b13b9
rosana <rosanariblim@gmail.com>
2022-01-16 18:25:46 -0300

    rota para LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  //note que login não é filha do navcomponent, pois o menu não deve aparecer na tela de login!
   {
     path:'', component: NavComponent, children:[
       {path:'home', component: HomeComponent},
