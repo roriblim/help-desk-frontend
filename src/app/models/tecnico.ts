3840144e236d3b7b33792dfe673679b05f42b5b7
rosana <rosanariblim@gmail.com>
2022-01-16 15:14:15 -0300

    html e css para TECNICO-LIST

---
+++ src/app/models/tecnico.ts
@@ -0,0 +1,9 @@
+export interface Tecnico{
+    id?: any; //passamos a interrogacao porque nem sempre teremos o id do tecnico. ex.: na hora de criar. o any é pq pode ser inteiro ou string.
+    nome: string;
+    cpf: string;
+    email: string;
+    senha: string;
+    perfis: string[];
+    dataCriacao: any; //colocamos any porque vamos trabalhar com string e com date
+}
