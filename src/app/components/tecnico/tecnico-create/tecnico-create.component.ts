d6bd933f651351fc6b74abe6005d7f3987170d76
rosana <rosanariblim@gmail.com>
2022-01-20 20:09:21 -0300

    validaçao dos formularios

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoCreateComponent implements OnInit {
 
+  nomeControl: FormControl = new FormControl(null, [Validators.minLength(3), Validators.maxLength(50)]);
+  cpfControl: FormControl = new FormControl(null, Validators.required);
+  emailControl: FormControl = new FormControl(null, Validators.email);
+  senhaControl: FormControl = new FormControl(null, Validators.minLength(3));
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos():boolean{
+    return this.nomeControl.valid &&
+            this.cpfControl.valid &&
+            this.emailControl.valid &&
+            this.senhaControl.valid;
   }
 
 }
