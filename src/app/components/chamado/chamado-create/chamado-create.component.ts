16eea04665543492b5a82be4d95fed0aae62fbf4
rosana <rosanariblim@gmail.com>
2022-01-24 18:03:51 -0300

    validacao de formularios para CHAMADO-CREATE

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chamado-create',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoCreateComponent implements OnInit {
 
+
+  prioridadeControl: FormControl = new FormControl(null, [Validators.required])
+  statusControl: FormControl = new FormControl(null, [Validators.required])
+  tituloControl: FormControl = new FormControl(null, [Validators.required])
+  descricaoControl: FormControl = new FormControl(null, [Validators.required])
+  tecnicoControl: FormControl = new FormControl(null, [Validators.required])
+  clienteControl: FormControl = new FormControl(null, [Validators.required])
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos():boolean{
+    return this.prioridadeControl.valid &&
+           this.statusControl.valid &&
+           this.tituloControl.valid &&
+           this.descricaoControl.valid &&
+           this.tecnicoControl.valid &&
+           this.clienteControl.valid;
   }
 
 }
